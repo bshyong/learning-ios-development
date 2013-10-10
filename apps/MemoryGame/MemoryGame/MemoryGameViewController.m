6317459ff26244a4a8f48849527e64baccbf374a
Ben Shyong <ben.shyong@gmail.com>
2013-10-10 03:10:44 -0400

    flipcard

--- apps/MemoryGame/MemoryGame/MemoryGameViewController.m
+++ apps/MemoryGame/MemoryGame/MemoryGameViewController.m
@@ -14,16 +14,9 @@
 
 @implementation MemoryGameViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (IBAction)flipCard:(UIButton *)sender {
+    sender.selected = !sender.isSelected;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
